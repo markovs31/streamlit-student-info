28991c0d7621e7c99649480b423954a60bc1c14e
markovs31 <stmarkovic@free.fr>
2026-04-16 12:19:17 +0200

    test graphe

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+# Import your libraries here
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+# Load the iris dataset
+df = sns.load_dataset('iris')
+
+# Violin plot for every feature
+sns.violinplot(data=df)
+fig = plt.gcf();
+
+
 # Title of the app
 st.title("Hello Streamlit 👋")
+
+
+
 student_name = st.text_input("Enter the student's name:", icon="🚨")
 # Slider for student's age
 student_age = st.slider("Select the student's age:", 1, 100)
